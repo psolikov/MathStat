9f5a253c0fcc4898e2f465e0117059876b0ba52b
Pavel Solikov <psolikov15@gmail.com>
2018-10-24 23:38:55 +0300

    Fixed

--- hw6/main.py
+++ hw6/main.py
@@ -14,33 +14,28 @@ def nmoment(x, i, n):
     return np.sum(x**i) / n
 
 for i in range(1, N):
-    s = 0
-    for j in range(1, i):
-        s += nmoment(np.random.normal(0, 1, i), 2, i) ** 2
-    x = 1 / (chi2.ppf((1 - gamma) / 2, i))
-    y = 1 / (chi2.ppf((1 + gamma) / 2, i))
-    s *= (x - y)
-    first.append(abs(s))
+    s = []
+    for j in range(0, 20):
+        x = 1 / (chi2.ppf((1 - gamma) / 2, i))
+        y = 1 / (chi2.ppf((1 + gamma) / 2, i))
+        s.append(abs(nmoment(np.random.normal(0, 1, i), 2, 1) * (x - y)))
+    first.append(np.mean(s))
 
     ###############
 
-    s = 0
-    for j in range(1, i):
-        s += nmoment(np.random.normal(0, 1, i), 2, i) ** 2
-    x = 1 / (norm.ppf((3 + gamma) / 4))
-    y = 1 / (norm.ppf((3 - gamma) / 4))
-    s *= (x * x - y * y)
-    second.append(abs(s))
+    s = []
+    for j in range(0, 20):
+        x = (norm.ppf((3 + gamma) / 4))
+        y = (norm.ppf((3 - gamma) / 4))
+        nprn = np.random.normal(0, 1, i)
+        s.append(abs(nmoment(nprn * nprn, 1, 1) * (1/(x * x * i) - 1/(y * y * i))))
+    second.append(np.mean(s))
 
 plt.plot([i for i in range(1, N)], first)
 
 plt.subplot(2, 1, 1)
-plt.title("Exponential (up) and uniform (down) distributions")
-plt.ylabel("Square of error's delta")
-plt.xlabel("Kth moment")
+
 plt.plot([i for i in range(1, N)], first)
 plt.subplot(2, 1, 2)
-plt.ylabel("Square of error's delta")
-plt.xlabel("Kth moment")
 plt.plot([i for i in range(1, N)], second)
 plt.show()
